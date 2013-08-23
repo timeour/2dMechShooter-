ec732729fb43c01bf60fce68d872d60b23bc6418
Sanekesv <sanekesv@gmail.com>
2013-08-23 14:39:00 +0400

    add musik

--- Assets/Scripts/Mov.cs
+++ Assets/Scripts/Mov.cs
@@ -5,6 +5,7 @@ public class Mov : MonoBehaviour
 {
 	public float speed;
 	private OTAnimatingSprite _mySprite;
+	bool ismov;
 
 	public OTAnimatingSprite MySprite {
 		get {
@@ -18,26 +19,18 @@ public class Mov : MonoBehaviour
 	// Use this for initialization
 	void Start ()
 	{
+		
 	}
 	
 	void Update ()
 	{
-		/*Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, , Input.mousePosition.y0f));
-		Debug.Log(mousePositionInWorld);
-		transform.LookAt (new Vector3 (mousePositionInWorld.x, transform.position.y, mousePositionInWorld.z));
-*/
 		Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint (new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0f));
 		float angle = Vector3.Angle (new Vector3 (mousePositionInWorld.x, mousePositionInWorld.y, transform.position.z) - transform.position, Vector3.right);
 		if (mousePositionInWorld.y < transform.position.y) {
 			transform.rotation = Quaternion.Euler (0f, 0f, -angle);
 		} else {
 			transform.rotation = Quaternion.Euler (0f, 0f, angle);
-		}
-		Debug.Log (angle);
-		
-//		Quaternion neededRotation = Quaternion.LookRotation (new Vector3 (mousePositionInWorld.x, mousePositionInWorld.y, transform.position.z) - transform.position);
-//		transform.rotation = neededRotation * Quaternion.Euler (0f, -90f, 0f);
-		//			.LookAt (new Vector3 (mousePositionInWorld.x, mousePositionInWorld.y, transform.position.z), transform.forward);
+		}		
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,18 +7,22 @@ public class Shooter : MonoBehaviour
 	public Transform bullet;
 	public int speed;
 	public float shotOffset;
+	public AudioClip fon;
+	public AudioClip fire;
 	
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+		audio.PlayOneShot(fon);
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+
 		if (Input.GetMouseButtonDown (0)) {
+			audio.PlayOneShot(fire);
 			Transform BulletInstance = (Transform)Instantiate (bullet, transform.position + (transform.right * shotOffset), transform.rotation);
 			BulletInstance.rigidbody.AddForce (transform.right * speed);
 		}
