50cdb31801a092b395be7b2860ebcb7dd0698616
Sanekesv <sanekesv@gmail.com>
2013-08-30 11:37:32 +0400

    postrock

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,31 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Spawner : MonoBehaviour {
+public class Spawner : MonoBehaviour
+{
 	private static int scoore = 0;
+	public GameObject[] robotPrefab;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
-		scoore = Bullet.getScore();
+		scoore = Bullet.getScore ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
-		if (scoore != Bullet.getScore()) {
-			Spawn();
-			scoore = Bullet.getScore();
+		if (scoore != Bullet.getScore ()) {
+			Spawn ();
+			scoore = Bullet.getScore ();
 		}
-		Debug.Log (scoore);
+		Debug.Log (scoore + " " + Bullet.getScore ());
 	}
-	void Spawn () {
+
+	void Spawn ()
+	{
 		int i, j, n;
 		i = Random.Range (-2, 2);
 		j = Random.Range (0, 4);
 		n = Random.Range (1, 9);
-		GameObject targetEnemy = GameObject.Find ("Enemy_" + n);
-		Debug.Log("Enemy_" + n);
-		Transform EnemyInstantiate = (Transform)Instantiate (targetEnemy, new Vector3(i*500f, j*500f, -50f), targetEnemy.rigidbody.rotation);
+		Transform newEnemy = (Instantiate (robotPrefab [Random.Range (0, robotPrefab.Length)], new Vector3 (i * 500f, j * 500f, -50f), Quaternion.identity) as GameObject).transform;
 	}
 }
